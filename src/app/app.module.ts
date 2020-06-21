4dfdad5f0222d16046bafa9967fcff9b2b7cb833
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-21 14:52:23 -0500

    todo module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
