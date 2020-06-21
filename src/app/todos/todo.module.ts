cc56e14fd7fcf330c238165cc1bd90c0b563c7d8
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-21 14:57:43 -0500

    todo-list component created

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
 
 @NgModule({
-  declarations: [AddTodoComponent, TodoFooterComponent, TodoItemComponent],
+  declarations: [AddTodoComponent, TodoFooterComponent, TodoItemComponent, TodoListComponent],
   imports: [
     CommonModule
   ]
