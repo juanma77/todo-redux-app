0cbbd3e4f313dc200b42d34efa0742fa2843733d
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-21 14:56:31 -0500

    todo-footer component created

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 
 
 
 @NgModule({
-  declarations: [AddTodoComponent],
+  declarations: [AddTodoComponent, TodoFooterComponent],
   imports: [
     CommonModule
   ]
