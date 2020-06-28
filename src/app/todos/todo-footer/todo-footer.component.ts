5467044d4c605e82aeae2f74419432a4cd007e3c
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-06-28 16:01:10 -0500

    changing filter functionality added

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import { filtrosValidos, SET_FILTRO_ACTION } from 'src/app/filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  public filtroActual: filtrosValidos = 'all';
+
+  public filtros: filtrosValidos[] = ['all', 'completed', 'pending'];
+
+  constructor( private store: Store<AppState> ) { }
 
   ngOnInit() {
+
+    this.store.select('filtro').subscribe( filtro =>{
+      
+      // console.log(filtro);
+
+      this.filtroActual = filtro; 
+
+    });
+
+  }
+
+  public cambiarFiltro( filtro: filtrosValidos ) {
+
+    // console.log(filtro);
+    
+    this.store.dispatch(SET_FILTRO_ACTION( { filtro: filtro } ))  
+
   }
 
 }
